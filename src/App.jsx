e1ffe88e412b115ae3c56dbf5abf1cd917102a95
Satyamchaubey9889 <Satyamchaubey9565@gmail.com>
2026-05-18 22:57:03 +0530

    add AdminHome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+  const location = useLocation();
+
+  const isAdminPage = location.pathname.startsWith("/admin");
+
   return (
     <>
-      <Navbar />
+      {!isAdminPage && <Navbar />}
 
       <AppRoutes />
 
-      <Footer />
+      {!isAdminPage && <Footer />}
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,13 @@ import Testimonials from "@/components/Testimonials";
 export default function Home() {
     return (
         <main className="bg-[#f9f9f9] text-slate-900 overflow-hidden">
-            <Navbar />
             <Hero />
             <Stats />
             <AboutSection />
             <CampusLife />
             <NewsAndEvents />
             <Academics />
-            <Testimonials/>
+            <Testimonials />
             <CTA />
 
         </main>
